0bc689dfc34e2d4e3845f4a3c475b6fb77fc35f4
DanilDer <dderyabin0302@gmail.com>
2024-02-20 02:07:28 -0500

    Second

--- inference_classifier.py
+++ inference_classifier.py
@@ -15,7 +15,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K'}
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L'}
 while True:
 
     data_aux = []
